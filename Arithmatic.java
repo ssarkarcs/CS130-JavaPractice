7e702d7248a7cfc9fcc18bc09cca91023da9b5e5
ssarkarcs <86372829+ssarkarcs@users.noreply.github.com>
2022-08-29 11:21:17 -0400

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,14 @@
+public class Arithmatic
+//this is our public class to do some arithmatic operations
+
+{
+public static void main(String[] args)
+//This is our main method
+{
+int number1=3; 
+int number2=5;
+int sum;
+sum=number1+number2;
+System.out.println("The summation is: " + sum);
+}
+}
